cf6a260e6d382f93c3280776f6e6481382a1f5d4
jonathanvdc <jonathan.vdc@outlook.com>
2019-06-12 12:36:47 +0200

    Support a bump allocator config

--- common/julia/cudanative.jl
+++ common/julia/cudanative.jl
@@ -10,6 +10,15 @@ macro cuda(ex...)
     esc(quote
         if !haskey(ENV, "JULIA_GC")
             CUDAnative.@cuda $(ex...)
+        elseif ENV["JULIA_GC"] == "BUMP"
+            local capacity = 8 * MiB
+            local buf = Mem.alloc(Mem.DeviceBuffer, capacity)
+            local start_address = pointer(buf)
+            local function init(kernel)
+                CUDAnative.Runtime.bump_alloc_init!(kernel, start_address, capacity)
+            end
+            @sync CUDAnative.@cuda init=init malloc="ptx_bump_alloc" $(ex...)
+            Mem.free(buf)
         elseif ENV["JULIA_GC"] == "NAIVE"
             CUDAnative.@cuda gc=true gc_config=naive_gc_config $(ex...)
         else
